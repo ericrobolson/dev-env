ae9cdb34aa93f823d5d7726789dfb7a3a00baf68
Eric Olson <eric.rob.olson@gmail.com>
2021-10-29 16:21:18 -0700

    Added in nix-shell

---
+++ default.nix
@@ -0,0 +1,33 @@
+with import <nixpkgs> {};
+
+stdenv.mkDerivation rec {
+    name = "env";
+    env = buildEnv { name = name; paths = buildInputs; };
+    buildInputs = [
+        asdf
+        # Editors
+        emacs
+        # Elixir + Erlang
+        erlang
+        elixir
+        # Haskell
+        ghc
+        haskellPackages.Cabal_3_4_0_0
+        cabal-install
+        # Idris 
+        idris
+        # JavaScript
+        yarn
+        # Lisp
+        sbcl
+        # Rust
+        cargo
+        rustc
+        rustfmt
+        # Scheme
+        chicken
+    ];
+
+    # Load env vars in shell.
+    shellHook = ''set -a; source .env;'';
+}
