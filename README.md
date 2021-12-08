2ba399fd375a3dac468ee34ec555410b95114674
Eric Olson <eric.rob.olson@gmail.com>
2021-12-07 18:21:10 -0800

    Added basic startup script.

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ To get started, run the following commands after getting into a `bash` shell:
 2) `sh hack.sh`
 
 Doing that will load everything and get it rolling.
+
+Check out the `startup.sh` script for a simple script that will pull down and clone the dev environment.  
---
+++ startup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# This script will clone the dev env, then launch it. 
+
+CUR_DIR=${PWD}
+DEV_ENV=.dev_env
+
+# Clone dev-env if it doesn't exist
+[ ! -d ${DEV_ENV} ] && git clone https://github.com/ericrobolson/dev-env.git ${DEV_ENV}
+
+# Update dev-env
+cd ${DEV_ENV}
+git checkout -- . 
+git pull
+
+# Run emacs
+sh hack.sh
